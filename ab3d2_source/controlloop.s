d841aef47f53624b35876716bdc91eaea794d830
0xABADCAFE <karlchurchill@gmail.com>
2022-08-25 20:39:06 +0100

    controlloop.s - cheese suppression

--- ab3d2_source/controlloop.s
+++ ab3d2_source/controlloop.s
@@ -184,12 +184,12 @@ START:
 				jsr		LOADFLOOR
 				jsr		LOADOBS
 
-				ifeq	CHEESEY
+;				ifeq	CHEESEY
 				move.l	#backpicname,a0
 				move.l	#BackPicture,d0
 				move.l	#0,d1
 				jsr		QUEUEFILE
-				endc
+;				endc
 
 ; jsr _StopPlayer
 ; jsr _RemPlayer
