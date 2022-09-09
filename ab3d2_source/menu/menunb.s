a5d4e8bc58138361d713023c2d4ddbc5dd69a9c4
mheyer <sonode@gmx.de>
2022-09-08 23:27:26 -0700

    Don't create 0 sized windows
    The OS really doesn't like it.
    This destroys the menu scrolling background over time, because
    it is "baked" into

--- ab3d2_source/menu/menunb.s
+++ ab3d2_source/menu/menunb.s
@@ -2205,14 +2205,10 @@ MenuScreen		dc.l	0
 
 WindowTags		dc.l	WA_Left,0
 				dc.l	WA_Top,0
-				dc.l	WA_Width,0
-				dc.l	WA_Height,0
+				dc.l	WA_Width,320
+				dc.l	WA_Height,256
 				dc.l	WA_CustomScreen
 WTagScreenPtr	dc.l	0						; will fill in screen pointer later
-				; intution.i states "WA_Flags ;not implemented at present"
-				; But I have seen code using it...
-				dc.l	WA_Flags,WFLG_ACTIVATE!WFLG_BORDERLESS!WFLG_RMBTRAP!WFLG_SIMPLE_REFRESH!WFLG_BACKDROP!WFLG_NOCAREREFRESH
-				; Just to be sure, provide the same info again
 				dc.l	WA_Activate,1
 				dc.l	WA_Borderless,1
 				dc.l	WA_RMBTrap,1			; prevent menu rendering
