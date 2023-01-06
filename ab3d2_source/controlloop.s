e230f6dd8b6a6e00c9e9e7dfbbd433cd0aceb5ba
AndyLoft <ltheden@aol.com>
2023-01-06 01:42:46 +0000

    bug fix - reset LOADEXT after loading deflev.dat

--- ab3d2_source/controlloop.s
+++ ab3d2_source/controlloop.s
@@ -572,6 +572,7 @@ DEFGAME:
 
 				move.l	DEFGAMEPOS,a1;		req?
 				CALLEXEC FreeVec;		req?
+				not.b	LOADEXT;			 reset for next load
 				bra	.defloaded;			 can use this now
 .error_nodef;								 can use this now
 				not.b	LOADEXT
