d46f0f1031068878fea7b8ade4dbd4fccd33a054
mheyer <sonode@gmx.de>
2023-05-17 00:45:28 -0700

    Install fake VBlank interrupt for RTG modes
    I don't think we can rely on the Amiga's VBL interrupt,
    when RTG modes are active(?)
    
    So for now as a hack create a 50hz timer interrupt.

--- ab3d2_source/c/system.c
+++ ab3d2_source/c/system.c
@@ -22,6 +22,7 @@
 #include <resources/potgo.h>
 
 #define ALIB_HARDWARE_CUSTOM
+#include <clib/alib_protos.h>
 #include <proto/alib.h>
 
 #define INTUITIONNAME "intuition.library"
@@ -53,9 +54,20 @@ extern VOID VBlankInterrupt(void);
 extern VOID key_interrupt(void);
 
 static const char AppName[] = "TheKillingGrounds";
-static struct Interrupt VBLANKInt = {{NULL, NULL, NT_INTERRUPT, 9, (char*)AppName}, 0, VBlankInterrupt};
-static struct Interrupt KBInt = {NULL, NULL, NT_INTERRUPT, 127, (char*)AppName, 0, key_interrupt};
+static struct Interrupt VBLANKInt = {{NULL, NULL, NT_INTERRUPT, 9, (char *)AppName}, 0, VBlankInterrupt};
+static struct Interrupt KBInt = {NULL, NULL, NT_INTERRUPT, 127, (char *)AppName, 0, key_interrupt};
 
+static struct MsgPort vblPort;
+static struct VBLData
+{
+    volatile ULONG tsi_Flag;
+    struct MsgPort *tsi_Port;
+} vblData = {0, &vblPort};
+
+static BOOL SAVEDS FakeVBlankInterrupt(REG(a1, struct VBLData *data));
+
+static struct Interrupt PseudoVBLANKInt = {
+    NULL, NULL, NT_INTERRUPT, 0, (char *)AppName, &vblData, &FakeVBlankInterrupt};
 
 static BOOL sys_InitHardware();
 static void sys_ReleaseHardware();
@@ -105,10 +117,32 @@ BOOL sys_OpenLibs(void)
     if (!(PotgoBase = OpenResource(POTGONAME))) {
         goto fail;
     }
-    if (OpenDevice(TIMERNAME, 0, &sys_TimerRequest.tr_node, 0) != 0) {
-        goto fail;
+
+    {
+        /* Set up the (software)interrupt structure. Note that this task runs at  */
+        /* priority 0. Software interrupts may only be priority -32, -16, 0, +16, */
+        /* +32. Also not that the correct node type for a software interrupt is   */
+        /* NT_INTERRUPT. (NT_SOFTINT is an internal Exec flag). This is the same  */
+        /* setup as that for a software interrupt which you Cause(). If our       */
+        /* interrupt code was in assembler, you could initialize is_Data here to  */
+        /* contain a pointer to shared data structures. An assembler software     */
+        /* interrupt routine would receive the is_Data in A1.                     */
+
+        vblPort.mp_Node.ln_Type = NT_MSGPORT;  /* Set up the PA_SOFTINT message port  */
+        vblPort.mp_Flags = PA_SOFTINT;         /* (no need to make this port public). */
+        vblPort.mp_SigTask = &PseudoVBLANKInt; /* pointer to interrupt structure */
+        NewList(&(vblPort.mp_MsgList));        /* Initialize message list */
+
+        struct Message *rplyMsg = &sys_TimerRequest.tr_node.io_Message;
+        rplyMsg->mn_Node.ln_Type = NT_REPLYMSG;
+        rplyMsg->mn_ReplyPort = &vblPort;
+        rplyMsg->mn_Length = sizeof(struct TimeRequest);
+
+        if (OpenDevice(TIMERNAME, UNIT_MICROHZ, &sys_TimerRequest.tr_node, 0) != 0) {
+            goto fail;
+        }
+        TimerBase = sys_TimerRequest.tr_node.io_Device;
     }
-    TimerBase = sys_TimerRequest.tr_node.io_Device;
 
     // optional
     CyberGfxBase = OpenLibrary(CYBERGFXNAME, CYBERGFX_INCLUDE_VERSION);
@@ -174,6 +208,73 @@ fail:
     return FALSE;
 }
 
+#define OFF 0
+#define ON 1
+#define STOPPED 2
+
+static void RemoveFakeVBlankInterrupt(void);
+
+static BOOL InstallFakeVBlankInterrupt(void)
+{
+    vblData.tsi_Flag = ON; /* Init data structure to share globally. */
+    vblData.tsi_Port = &vblPort;
+
+    /* Send of the first timerequest to start. IMPORTANT: Do NOT   */
+    /* BeginIO() to any device other than audio or timer from      */
+    /* within a software or hardware interrupt. The BeginIO() code */
+    /* may allocate memory, wait or perform other functions which  */
+    /* are illegal or dangerous during interrupts.                 */
+    sys_TimerRequest.tr_node.io_Command = TR_ADDREQUEST; /* Initial iorequest to start */
+    sys_TimerRequest.tr_time.tv_micro = 20000;           /* 50Hz software interrupt.        */
+    BeginIO(&sys_TimerRequest.tr_node);
+
+    return TRUE;
+fail:
+    RemoveFakeVBlankInterrupt();
+
+    return FALSE;
+}
+
+static void RemoveFakeVBlankInterrupt(void)
+{
+    LOCAL_SYSBASE();
+
+    if (vblData.tsi_Flag == ON) {
+        // Wait for interrupt to cease creating new IO requests
+        vblData.tsi_Flag = OFF;
+        while (vblData.tsi_Flag != STOPPED) {
+            Delay(1);
+        };
+        vblData.tsi_Flag = OFF;
+    }
+}
+
+static BOOL SAVEDS FakeVBlankInterrupt(REG(a1, struct VBLData *data))
+{
+    LOCAL_SYSBASE();
+
+    struct timerequest *tr;
+
+    /* Remove the message from the port. */
+    tr = (struct timerequest *)GetMsg(data->tsi_Port);
+
+    /* Keep on going if flag hasn't been set to OFF. */
+    if ((tr) && (data->tsi_Flag == ON)) {
+        /* Re-send timerequest--IMPORTANT: This         */
+        /* self-perpetuating technique of calling BeginIO() during a software */
+        /* interrupt may only be used with the audio and timer device.        */
+        tr->tr_node.io_Command = TR_ADDREQUEST;
+        tr->tr_time.tv_micro = 20000;
+        BeginIO((struct IORequest *)tr);
+
+        CallAsm(&VBlankInterrupt);
+    } else {
+        data->tsi_Flag = STOPPED;
+    }
+
+    return TRUE;
+}
+
 void sys_ReleaseHardware()
 {
     if (gotSerialBits) {
@@ -192,14 +293,22 @@ void sys_InstallInterrupts()
 {
     LOCAL_SYSBASE();
     //    AddVBlankInt(VBlankInterrupt, 0); // lowlevel.library
-    AddIntServer(INTB_VERTB, &VBLANKInt);
+    if (!vid_isRTG) {
+        AddIntServer(INTB_VERTB, &VBLANKInt);
+    } else {
+        InstallFakeVBlankInterrupt();
+    }
     AddIntServer(INTB_PORTS, &KBInt);
 }
 
 void sys_RemoveInterrupts()
 {
     LOCAL_SYSBASE();
-    RemIntServer(INTB_VERTB, &VBLANKInt);
+    if (!vid_isRTG) {
+        RemIntServer(INTB_VERTB, &VBLANKInt);
+    } else {
+        RemoveFakeVBlankInterrupt();
+    }
     RemIntServer(INTB_PORTS, &KBInt);
 }
 
