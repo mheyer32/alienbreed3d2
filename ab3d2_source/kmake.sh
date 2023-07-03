8787fcdfc14a92fff577a262bf9250825636b97a
0xABADCAFE <karlchurchill@gmail.com>
2023-07-03 10:04:36 +0100

    Parking up.

--- ab3d2_source/kmake.sh
+++ ab3d2_source/kmake.sh
@@ -1,4 +1,19 @@
-#!/bin/bash
-rm hires
-make hires_stripped
-cp hires_stripped ../../../miscdrive/karlos-tkg/Game/tkg
+# This script is for building the binaries in the karlos-tkg mod. Probably needs removing from this repo.
+
+rm ../../../miscdrive/karlos-tkg/Game/bin/*
+make clean
+make hires hiresC
+
+# Make sure they are stripped
+m68k-amigaos-strip hires
+m68k-amigaos-strip hiresC
+
+cp hires ../../../miscdrive/karlos-tkg/Game/bin/tkg_asm
+cp hiresC ../../../miscdrive/karlos-tkg/Game/bin/tkg_c
+
+make clean
+
+make dev 
+cp hires ../../../miscdrive/karlos-tkg/Game/bin/tkg_asm_dev
+cp hiresC ../../../miscdrive/karlos-tkg/Game/bin/tkg_c_dev
+
