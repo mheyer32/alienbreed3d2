8dede832e671792de98ca0b9438a918935b8154e
0xABADCAFE <karlchurchill@gmail.com>
2024-09-02 18:36:08 +0100

    A selection of crosshair colours

--- ab3d2_source/modules/draw.s
+++ ab3d2_source/modules/draw.s
@@ -385,37 +385,37 @@ Draw_Crosshair:
 
 				muls.w  #(256/9),d1
 				asr.l   #8,d1
-
 				and.b   #$fe,d1
-
 				muls.w  #SCREEN_WIDTH,d1
 
 				add.w d1,d0
 				bra.s	.below
 
-.above
-;				move.w	 STOPOFFSET,d1
+.above:
 				neg.w	d1
-				;divs.w #9,d1
-				;muls.w #SCREEN_WIDTH,d1
 
 				muls.w  #(256/9),d1
 				asr.l   #8,d1
-
                 and.b   #$fe,d1
-
-
 				muls.w  #SCREEN_WIDTH,d1
 
 				sub.w d1,d0
 
-.below
+.below:
 ***************************************************************
-.small
+.small:
 
 				add.l	d0,a0
+                clr.l   d0
+				move.b  Prefs_CrossHairColour_b,d0
+				and.b   #7,d0 ; paranoia
+				move.l  #Draw_CrosshairPens_vb,a1
+				add.w   d0,a1
+				move.b  (a1),d0
 
-				move.b  #254,d0
+				; TODO - Mod Properties should define a mechanism to allow per gun
+				;        crosshair designs
+
 				move.b	d0,-4*SCREEN_WIDTH-4(a0) ; TL
 				move.b	d0,-4*SCREEN_WIDTH+4(a0) ; TR
                 move.b	d0,-2*SCREEN_WIDTH-2(a0) ; TL
@@ -426,6 +426,16 @@ Draw_Crosshair:
 				move.b	d0,4*SCREEN_WIDTH-4(a0) ; BL
 				move.b	d0,4*SCREEN_WIDTH+4(a0) ; BR
 
-
-
 				rts
+
+				; TODO - this should be dedefinable by mod and/or user as they are currently defined
+				; by the default palette
+Draw_CrosshairPens_vb:
+                dc.b 255 ; intense green
+                dc.b 254 ; mid green
+                dc.b 190 ; intense yellow
+                dc.b  25 ; bright grey
+                dc.b 250 ; intense red
+                dc.b 133 ; ice blue
+                dc.b  69 ; intense blue
+                dc.b   0 ; black - for use in bright environments
