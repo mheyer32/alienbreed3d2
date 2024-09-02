2d5fbc3b209bc9fb6f34b7133a23de054ef9d1c9
0xABADCAFE <karlchurchill@gmail.com>
2024-09-03 00:22:53 +0100

    Added no cross hair to selection cycle

--- ab3d2_source/modules/draw.s
+++ ab3d2_source/modules/draw.s
@@ -370,6 +370,16 @@ Draw_NarrateText:
 				rts
 
 Draw_Crosshair:
+                ; Get the pen
+                clr.l   d0
+				move.b  Prefs_CrossHairColour_b,d0
+				and.b   #7,d0 ; paranoia
+				move.l  #Draw_CrosshairPens_vb,a1
+				add.w   d0,a1
+				tst.b   (a1)
+                beq     .done
+
+
 				move.l	Vid_FastBufferPtr_l,a0
 				add.w	Vid_CentreX_w,a0
 				move.w	Vid_BottomY_w,d0
@@ -404,13 +414,8 @@ Draw_Crosshair:
 .below:
 ***************************************************************
 .small:
-
 				add.l	d0,a0
-                clr.l   d0
-				move.b  Prefs_CrossHairColour_b,d0
-				and.b   #7,d0 ; paranoia
-				move.l  #Draw_CrosshairPens_vb,a1
-				add.w   d0,a1
+
 				move.b  (a1),d0
 
 				; TODO - Mod Properties should define a mechanism to allow per gun
@@ -425,7 +430,7 @@ Draw_Crosshair:
 				move.b	d0,2*SCREEN_WIDTH+2(a0) ; BR
 				move.b	d0,4*SCREEN_WIDTH-4(a0) ; BL
 				move.b	d0,4*SCREEN_WIDTH+4(a0) ; BR
-
+.done:
 				rts
 
 				; TODO - this should be dedefinable by mod and/or user as they are currently defined
@@ -438,4 +443,4 @@ Draw_CrosshairPens_vb:
                 dc.b 250 ; intense red
                 dc.b 133 ; ice blue
                 dc.b  69 ; intense blue
-                dc.b   0 ; black - for use in bright environments
+                dc.b   0 ; off
