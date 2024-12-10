707190bd95ea8ab907a7a954f283b7133400561e
0xABADCAFE <karlchurchill@gmail.com>
2024-12-10 00:57:38 +0000

    Identify when to skip the upper or lower part of the root zone

--- ab3d2_source/bss/draw_bss.s
+++ ab3d2_source/bss/draw_bss.s
@@ -109,6 +109,7 @@ draw_DownStrip_w:				ds.w	1
 draw_WhichDoing_b:				ds.b	1
 draw_InUpperZone_b:				ds.b	1
 Draw_DoUpper_b:					ds.b	1
+Draw_SkipZoneLevel_b:				ds.b	1
 
 		DCLC Draw_ForceSimpleWalls_b,	ds.b,	1
 		DCLC Draw_ForceZoneSkip_b,		ds.b,	1
