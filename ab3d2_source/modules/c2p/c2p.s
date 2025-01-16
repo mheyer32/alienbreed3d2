0aa88b3199fbcd4a29e00d8c239acd4d96378a83
0xABADCAFE <karlchurchill@gmail.com>
2025-01-16 20:34:38 +0000

    C2P function selection

--- ab3d2_source/modules/c2p/c2p.s
+++ ab3d2_source/modules/c2p/c2p.s
@@ -2,28 +2,104 @@
 ; Main C2P entry points
 ;
 
-				section .text,code
-				align 4
 
 FS_C2P_HEIGHT equ FS_HEIGHT-FS_HEIGHT_C2P_DIFF
 
-; TODO lookup table of available routines and a simplified selection
+; Table structure for C2P callbacks
 
+; Bit#   3                2                   1                  0
+; < 040+ / 030 > | < Full / Small > | < Double Width > | < Double Height >
+
+				section .data,data
+				align 4
+
+c2p_SetParamsPtrs_vl:
+				; Only include 030 code for the generic target
+				IFND OPT060
+				IFND OPT040
+
+				dc.l	c2p_SetParamsSmall1x1Opt030		; 0000
+				dc.l	c2p_SetParamsSmall1x2Opt030		; 0001
+				dc.l	c2p_SetParamsNull				; 0010
+				dc.l	c2p_SetParamsNull				; 0011
+				dc.l	c2p_SetParamsFull1x1Opt030		; 0100
+				dc.l	c2p_SetParamsFull1x2Opt030		; 0101
+				dc.l	c2p_SetParamsNull				; 0110
+				dc.l	c2p_SetParamsNull				; 0111
+
+				ENDC
+				ENDC
+
+				dc.l	c2p_SetParamsSmall1x1Opt040		; 1000
+				dc.l	c2p_SetParamsSmall1x2Opt040		; 1001
+				dc.l	c2p_SetParamsNull				; 1010
+				dc.l	c2p_SetParamsNull				; 1011
+				dc.l	c2p_SetParamsFull1x1Opt040		; 1100
+				dc.l	c2p_SetParamsFull1x2Opt040		; 1101
+				dc.l	c2p_SetParamsNull				; 1110
+				dc.l	c2p_SetParamsNull				; 1111
+
+c2p_ConvertPtrs_vl:
+				IFND OPT060
+				IFND OPT040
+
+				; Only include 030 code for the generic target
+				dc.l	c2p_ConvertSmall1x1Opt030		; 0000
+				dc.l	c2p_ConvertSmall1x2Opt030		; 0001
+				dc.l	c2p_ConvertNull					; 0010
+				dc.l	c2p_ConvertNull					; 0011
+				dc.l	c2p_ConvertFull1x1Opt030		; 0100
+				dc.l	c2p_ConvertFull1x2Opt030		; 0101
+				dc.l	c2p_ConvertNull					; 0110
+				dc.l	c2p_ConvertNull					; 0111
+
+				ENDC
+				ENDC
+
+				dc.l	c2p_ConvertSmall1x1Opt040		; 1000
+				dc.l	c2p_ConvertSmall1x2Opt040		; 1001
+				dc.l	c2p_ConvertNull					; 1010
+				dc.l	c2p_ConvertNull					; 1011
+				dc.l	c2p_ConvertFull1x1Opt040		; 1100
+				dc.l	c2p_ConvertFull1x2Opt040		; 1101
+				dc.l	c2p_ConvertNull					; 1110
+				dc.l	c2p_ConvertNull					; 1111
+
+				section .text,code
+
+c2p_SetParamsNull:
+c2p_ConvertNull:
+				rts
 
 
 ; Main C2P Initialisation
 	DCLC C2P_Init
-				tst.b	Vid_FullScreenTemp_b
-				beq.s	.small
+				moveq	#0,d1
+				move.b	Vid_DoubleHeight_b,d1
+				andi.b	#1,d1
+				move.b	Vid_DoubleWidth_b,d0
+				andi.b	#2,d0
+				or.b	d0,d1
+				move.b	Vid_FullScreenTemp_b,d0
+				andi.b	#4,d0
+				or.b	d0,d1
 
-				move.l	#c2p_SetParamsFull1x2Opt030,Vid_C2PSetParamsPtr_l
-				move.l	#c2p_ConvertFull1x2Opt030,Vid_C2PConvertPtr_l
-				bra.s	.done_size
-.small:
-				move.l	#c2p_SetParamsSmall1x2Opt030,Vid_C2PSetParamsPtr_l
-				move.l	#c2p_ConvertSmall1x2Opt030,Vid_C2PConvertPtr_l
+				; Only perform CPU check in the generic target, else use 040+ always
+				IFND OPT060
+				IFND OPT040
 
-.done_size:
+				move.b	Sys_Move16_b,d0
+				andi.b	#8,d0
+				or.b	d0,d1
+
+				ENDC
+				ENDC
+
+				; d1 should now contain all the bits needed to select the variant
+				move.l	#c2p_SetParamsPtrs_vl,a0
+				move.l	(a0,d1.w*4),Vid_C2PSetParamsPtr_l
+				move.l	#c2p_ConvertPtrs_vl,a0
+				move.l	(a0,d1.w*4),Vid_C2PConvertPtr_l
 				st		C2P_NeedsSetParam_b
 				clr.b	C2P_NeedsInit_b
 				rts
@@ -50,7 +126,14 @@ FS_C2P_HEIGHT equ FS_HEIGHT-FS_HEIGHT_C2P_DIFF
 				jsr	(a0)
 				rts
 
+				IFND OPT060
+				IFND OPT040
+
 				include "modules/c2p/68030/c2p.s"
+
+				ENDC
+				ENDC
+
 				include "modules/c2p/68040/c2p.s"
 
 C2P_NeedsInit_b:
