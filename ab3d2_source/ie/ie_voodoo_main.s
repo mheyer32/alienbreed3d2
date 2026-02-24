618a0e6e171fbde59680086162cc8fc052bf1ba8
Zayn Otley <intuitionamiga@gmail.com>
2026-02-24 23:00:02 +0100

    1. Added a dedicated Voodoo renderer module: ie_voodoo_render.s (/home/zayn/GolandProjects/alienbreed3d2/ab3d2_source/ie/      ie_voodoo_render.s)   2. Reworked Voodoo entrypoint to use the renderer module and game/input loop: ie_voodoo_main.s (/home/zayn/GolandProjects/      alienbreed3d2/ab3d2_source/ie/ie_voodoo_main.s)
      Key behavior now:
    
      - Initializes Voodoo at 640x480.
      - Sets clip rect using exclusive right/bottom bounds.
      - Configures FBZ mode with clipping + depth test/write + RGB write.
      - Clears color+depth via FAST_FILL_CMD every frame.
      - Submits layered triangles with different Z to exercise depth ordering.
      - Swaps buffers with vsync each frame.
      - Runs through existing ie_game_frame + ie_poll_input loop.
    
      Validation:
    
      - make ie68_voodoo passes and produces ab3d2_ie68_voodoo.ie68.
      - Smoke run boots with no M68K faults:
          - go run . -m68k ../alienbreed3d2/ab3d2_source/ab3d2_ie68_voodoo.ie68 (timed run in headless shell showed clean startup).

--- ab3d2_source/ie/ie_voodoo_main.s
+++ ab3d2_source/ie/ie_voodoo_main.s
@@ -1,45 +1,24 @@
-; ie_voodoo_main.s - Intuition Engine Voodoo bootstrap (WIP)
+; ie_voodoo_main.s - Intuition Engine Voodoo bootstrap
 
 	org $1000
 
 	xdef start
+	xref ie_voodoo_init
+	xref ie_voodoo_render_frame
 
 start:
-	; Enable IE video/compositor path.
+	; Enable IE compositor path.
 	move.l	#1,$F0000
 	move.l	#0,$F0004
 
-	; Configure Voodoo output dimensions (640x480) and enable engine.
-	move.l	#$028001E0,$F4214
-	move.l	#1,$F4004
-
 	bsr	ie_mem_init
 	bsr	ie_init
+	bsr	ie_voodoo_init
 
 main_loop:
+	bsr	ie_game_frame
 	bsr	ie_poll_input
-
-	; Clear to black via COLOR0 + FAST_FILL.
-	move.l	#0,$F41D8
-	move.l	#0,$F4124
-
-	; Flat red triangle (12.4 coords, 12.12 color, 20.12 depth).
-	move.l	#$00000640,$F4008	; AX = 100
-	move.l	#$00000320,$F400C	; AY = 50
-	move.l	#$00001900,$F4010	; BX = 400
-	move.l	#$00000640,$F4014	; BY = 100
-	move.l	#$00000A00,$F4018	; CX = 160
-	move.l	#$00001400,$F401C	; CY = 320
-
-	move.l	#$00001000,$F4020	; R = 1.0
-	move.l	#0,$F4024		; G = 0.0
-	move.l	#0,$F4028		; B = 0.0
-	move.l	#$00000800,$F402C	; Z = 0.5
-	move.l	#$00001000,$F4030	; A = 1.0
-
-	move.l	#0,$F4080		; TRIANGLE_CMD
-	move.l	#1,$F4128		; SWAP_BUFFER_CMD (vsync)
-
+	bsr	ie_voodoo_render_frame
 	bsr	ie_wait_vblank
 	bra	main_loop
 
@@ -51,3 +30,5 @@ main_loop:
 	include "ie_res.s"
 	include "ie_game.s"
 	include "ie_present.s"
+	include "ie_voodoo_render.s"
+	include "ie_compat.s"
