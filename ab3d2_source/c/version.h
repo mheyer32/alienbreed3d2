c752b82834267de5fbca36797c60751e4c5303f0
0xABADCAFE <karlchurchill@gmail.com>
2026-05-31 16:28:36 +0100

    Bumped version target to 1.2 since 1.1 already exists.

--- ab3d2_source/c/version.h
+++ ab3d2_source/c/version.h
@@ -1,6 +1,6 @@
 #ifndef VERSION_H
 #   define TKG_VERSION 1
-#   define TKG_REVISION 1
+#   define TKG_REVISION 2
 #   define TKG_BUILD 0
-#   define TKG_HASH "b77c9ad"
+#   define TKG_HASH "b704ec8"
 #endif
