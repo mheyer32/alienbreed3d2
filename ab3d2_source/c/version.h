640fc6de50ac5b398549ce0be81a1e8774f88ce5
0xABADCAFE <karlchurchill@gmail.com>
2026-06-05 20:19:58 +0100

    merge bump

--- ab3d2_source/c/version.h
+++ ab3d2_source/c/version.h
@@ -1,6 +1,6 @@
 #ifndef VERSION_H
 #   define TKG_VERSION 1
 #   define TKG_REVISION 2
-#   define TKG_BUILD 1
-#   define TKG_HASH "9408f60"
+#   define TKG_BUILD 2
+#   define TKG_HASH "269be6d"
 #endif
