9e67d7096c137ecef33eea871b47a491170576a4
0xABADCAFE <karlchurchill@gmail.com>
2026-06-20 11:11:17 +0100

    Release candidate.

--- ab3d2_source/c/version.h
+++ ab3d2_source/c/version.h
@@ -1,6 +1,6 @@
 #ifndef VERSION_H
 #   define TKG_VERSION 1
 #   define TKG_REVISION 2
-#   define TKG_BUILD 3
-#   define TKG_HASH "c913fdf"
+#   define TKG_BUILD 5
+#   define TKG_HASH "2e61338"
 #endif
